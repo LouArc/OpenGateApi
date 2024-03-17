21e5e42e301dfb6f253c1d01666dcc0f911ee6a4
ArcL <lou.arc.la@gmail.com>
2024-03-17 12:59:58 -0600

    Added promises and gate status function

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { generateCookie, openGate } = require("./services/beresident");
+const {
+  generateCookie,
+  openGate,
+  gateStatus,
+} = require("./services/beresident");
 
 const app = express();
 
@@ -21,15 +25,34 @@ app.post("/api/generatecookie", async (req, res) => {
   }
 });
 
-app.post("/api/opengate", async (req, res) => {
-  try {
-    const { token, id } = req.body; // Assuming user and password are sent in the request body
-    await openGate(token, id);
-    res.json({ message: "Gate opened" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+app.post("/api/opengate", (req, res) => {
+  const { token, id } = req.body; // Assuming user and password are sent in the request body
+  openGate(token, id)
+    .then(() => {
+      //call our gateStatus function
+      gateStatus(token, id)
+        .then((status) => {
+          res.json({ message: status });
+        })
+        .catch((error) => {
+          res.status(500).json({ error: error.message });
+        });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 });
+
+// app.post("/api/gateStatus", (req, res) => {
+//   const { token, id } = req.body; // Assuming user and password are sent in the request body
+//   gateStatus(token, id)
+//     .then((status) => {
+//       res.json({ message: status });
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error: error.message });
+//     });
+// });
 
 app.get("*", (req, res) => {
   res.status(404).json({ error: "Route not found" });
--- api/services/beresident.js
+++ api/services/beresident.js
@@ -39,36 +39,78 @@ async function generateCookie(user, password) {
 }
 
 async function openGate(token, id) {
-  const response = await fetch(
-    `https://app.beresident.mx/accesos/open2/${id}.json`,
-    {
-      headers: {
-        accept: "*/*",
-        "accept-language": "en,es;q=0.9",
-        "sec-ch-ua":
-          '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-requested-with": "XMLHttpRequest",
-        Cookie: `"CAKEPHP=${token}"`,
-      },
-      referrer: "https://app.beresident.mx/usuarios/accesos",
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: null,
-      method: "POST",
-      mode: "cors",
-      credentials: "include",
+  return new Promise((resolve, reject) => {
+    const response = fetch(
+      `https://app.beresident.mx/accesos/open2/${id}.json`,
+      {
+        headers: {
+          accept: "*/*",
+          "accept-language": "en,es;q=0.9",
+          "sec-ch-ua":
+            '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "x-requested-with": "XMLHttpRequest",
+          Cookie: `"CAKEPHP=${token}"`,
+        },
+        referrer: "https://app.beresident.mx/usuarios/accesos",
+        referrerPolicy: "strict-origin-when-cross-origin",
+        body: null,
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+      }
+    );
+
+    if (!response.ok) {
+      reject(new Error("Failed to open gate"));
     }
-  );
 
-  if (!response.ok) {
-    throw new Error("Failed to open gate");
-  }
-
-  return response.json();
+    resolve(response.json());
+  });
 }
 
-module.exports = { generateCookie, openGate };
+async function gateStatus(id, token) {
+  return new Promise((resolve, reject) => {
+    const response = fetch(
+      `https://app.beresident.mx/accesos/status/${id}.json`,
+      {
+        headers: {
+          accept: "*/*",
+          "accept-language": "en,es;q=0.9",
+          "sec-ch-ua":
+            '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+          "sec-ch-ua-arch": '"x86"',
+          "sec-ch-ua-bitness": '"64"',
+          "sec-ch-ua-full-version": '"122.0.6261.129"',
+          "sec-ch-ua-full-version-list":
+            '"Chromium";v="122.0.6261.129", "Not(A:Brand";v="24.0.0.0", "Google Chrome";v="122.0.6261.129"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-model": '""',
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-ch-ua-platform-version": '"15.0.0"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "x-requested-with": "XMLHttpRequest",
+          cookie: token,
+          Referer: "https://app.beresident.mx/usuarios/accesos",
+          "Referrer-Policy": "strict-origin-when-cross-origin",
+        },
+        body: null,
+        method: "POST",
+      }
+    );
+
+    if (!response.ok) {
+      reject(new Error("Failed to get gate status"));
+    }
+
+    resolve(response.json());
+  });
+}
+
+module.exports = { generateCookie, openGate, gateStatus };
